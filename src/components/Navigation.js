f7e4582c123338af0489c8c2f36c16f1155e5ce7
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-09-21 16:43:51 +0530

    crop navigator step flow added

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+
+class Navigation extends Component {
+  render() {
+    return (
+        <div>
+        <AppBar
+          title="Scan Document"
+        />
+        </div>
+    );
+  }
+}
+
+export default Navigation;
